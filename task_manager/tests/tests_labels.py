6416105c982be5ba8d00ebc6e02c6a205ff5b0ee
Eugene Anisimov <zero0061@mail.ru>
2024-11-12 20:13:27 +0200

    fix test status and label

--- task_manager/tests/tests_labels.py
+++ task_manager/tests/tests_labels.py
@@ -1,33 +1,27 @@
 from django.test import TestCase
 from django.urls import reverse
-from task_manager.users.models import User
 from task_manager.labels.models import Label
-from task_manager.tasks.models import Task
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
 
+
 # Create your tests here.
 class LabelTests(TestCase):
-    
     fixtures = ['labels.json', 'tasks.json', 'users.json', 'statuses.json']
-    
+
     def setUp(self):
         pass
 
-        
     def test_create_label(self):
         response = self.client.get(reverse('labels_create'), follow=True)
         self.assertRedirects(response, reverse('login'))
         self.assertContains(
             response, _("You are not logged in! Please log in.")
         )
-        
         self.client.force_login(get_user_model().objects.get(pk=1))
         response = self.client.get(reverse('labels_create'), follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'form.html')
-        
-        
         response = self.client.post(
             reverse('labels_create'),
             {'name': 'new_label'}
@@ -35,7 +29,7 @@ class LabelTests(TestCase):
         self.assertRedirects(response, reverse('labels_index'))
         self.assertEqual(response.status_code, 302)
         self.assertTrue(Label.objects.filter(name='new_label').exists())
-        
+
     def test_label_index(self):
         response = self.client.get(reverse('labels_index'), follow=True)
         self.assertRedirects(response, reverse('login'))
@@ -49,9 +43,35 @@ class LabelTests(TestCase):
         self.assertEqual(len(response.context['labels']), 2)
         self.assertContains(response, "with task")
         self.assertContains(response, "without task")
-    
+
     def test_update_label(self):
-        pass
-    
+        self.client.force_login(get_user_model().objects.get(pk=2))
+        label = Label.objects.get(pk=1)
+        response = self.client.get(reverse('labels_update',
+                                           args=[label.pk]),
+                                   follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'form.html')
+        response = self.client.post(
+            reverse('labels_update', args=[label.pk]),
+            {'name': 'updatedlabel'}
+        )
+        self.assertRedirects(response, reverse('labels_index'))
+        self.assertEqual(response.status_code, 302)
+        label.refresh_from_db()
+        self.assertEqual(label.name, 'updatedlabel')
+
     def test_delete_label(self):
-        pass
+        self.client.force_login(get_user_model().objects.get(pk=2))
+        label = Label.objects.get(pk=2)
+        response = self.client.post(reverse('labels_delete',
+                                            args=[label.pk]),
+                                    follow=True)
+        self.assertRedirects(response, reverse('labels_index'))
+        self.assertFalse(Label.objects.filter(pk=label.pk).exists())
+        label = Label.objects.get(pk=1)
+        response = self.client.post(reverse('labels_delete',
+                                            args=[label.pk]),
+                                    follow=True)
+        self.assertRedirects(response, reverse('labels_index'))
+        self.assertTrue(Label.objects.filter(pk=label.pk).exists())
--- task_manager/tests/tests_statuses.py
+++ task_manager/tests/tests_statuses.py
@@ -1,44 +1,77 @@
-# from django.test import TestCase
-# from django.urls import reverse
-# from task_manager.users.models import User
-# from ..statuses.models import Status
+from django.test import TestCase
+from django.urls import reverse
+from task_manager.statuses.models import Status
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
 
-# # Create your tests here.
-# class StatusesTests(TestCase):
+# Create your tests here.
+class LabelTests(TestCase):
+    fixtures = ['labels.json', 'tasks.json', 'users.json', 'statuses.json']
 
-#     def setUp(self):
-#         self.user = User.objects.create_user(username='test', password='test')
-#         self.client.login(username='test', password='test')
+    def setUp(self):
+        pass
 
-#     def test_create_status(self):
-#         response = self.client.post(
-#             reverse('statuses_create'),
-#             {'name': 'status'}
-#         )
-#         self.assertEqual(response.status_code, 302)
-#         self.assertTrue(Status.objects.filter(name='status').exists())
+    def test_create_label(self):
+        response = self.client.get(reverse('statuses_create'), follow=True)
+        self.assertRedirects(response, reverse('login'))
+        self.assertContains(
+            response, _("You are not logged in! Please log in.")
+        )
+        self.client.force_login(get_user_model().objects.get(pk=1))
+        response = self.client.get(reverse('statuses_create'), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'form.html')
+        response = self.client.post(
+            reverse('statuses_create'),
+            {'name': 'new_status'}
+        )
+        self.assertRedirects(response, reverse('statuses_index'))
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Status.objects.filter(name='new_status').exists())
 
-#     def test_update_status(self):
-#         status = Status.objects.create(name='status')
-#         response = self.client.post(
-#             reverse('statuses_update',
-#                     args=[status.pk]),
-#             {'name': 'status1'}
-#         )
-#         self.assertEqual(response.status_code, 302)
-#         status.refresh_from_db()
-#         self.assertEqual(status.name, 'status1')
+    def test_label_index(self):
+        response = self.client.get(reverse('statuses_index'), follow=True)
+        self.assertRedirects(response, reverse('login'))
+        self.assertContains(
+            response, _("You are not logged in! Please log in.")
+        )
+        self.client.force_login(get_user_model().objects.get(pk=1))
+        response = self.client.get(reverse('statuses_index'), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'statuses/index.html')
+        self.assertEqual(len(response.context['statuses']), 2)
+        self.assertContains(response, "with task")
+        self.assertContains(response, "without task")
 
-#     def test_delete_status(self):
-#         status = Status.objects.create(name='Delete')
-#         response = self.client.post(
-#             reverse('statuses_delete',
-#                     args=[status.pk])
-#         )
-#         self.assertEqual(response.status_code, 302)
-#         self.assertFalse(Status.objects.filter(name='Delete').exists())
+    def test_update_label(self):
+        self.client.force_login(get_user_model().objects.get(pk=2))
+        status = Status.objects.get(pk=1)
+        response = self.client.get(reverse('statuses_update',
+                                           args=[status.pk]),
+                                   follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'form.html')
+        response = self.client.post(
+            reverse('statuses_update', args=[status.pk]),
+            {'name': 'updatedstatus'}
+        )
+        self.assertRedirects(response, reverse('statuses_index'))
+        self.assertEqual(response.status_code, 302)
+        status.refresh_from_db()
+        self.assertEqual(status.name, 'updatedstatus')
 
-#     def test_index_view(self):
-#         response = self.client.get(reverse('statuses_index'))
-#         self.assertEqual(response.status_code, 200)
+    def test_delete_label(self):
+        self.client.force_login(get_user_model().objects.get(pk=2))
+        label = Status.objects.get(pk=2)
+        response = self.client.post(reverse('statuses_delete',
+                                            args=[label.pk]),
+                                    follow=True)
+        self.assertRedirects(response, reverse('statuses_index'))
+        self.assertFalse(Status.objects.filter(pk=label.pk).exists())
+        label = Status.objects.get(pk=1)
+        response = self.client.post(reverse('statuses_delete',
+                                            args=[label.pk]),
+                                    follow=True)
+        self.assertRedirects(response, reverse('statuses_index'))
+        self.assertTrue(Status.objects.filter(pk=label.pk).exists())
